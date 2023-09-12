fa9ed11c1cdf5a79658fbba8318e51695d185c54
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2023-09-12 10:26:15 +0530

    setup env for secret key

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import HTTPException, Security, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
@@ -5,15 +6,24 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from typing import Union
 from jose import jwt
+from dotenv import load_dotenv
 from database.models import User
 from database.database import get_db
+
+load_dotenv()
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 ALGORITHM = "HS256"
+
+
+if SECRET_KEY is None:
+    raise EnvironmentError("SECRET_KEY environment variable is missing")
 
 
 def verify_password(plain_password, hashed_password):
